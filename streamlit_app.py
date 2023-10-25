287c11a73adb9d97376c32b607032fef1bb24657
Hamid Bekamiri <hamid.bekam@gmail.com>
2023-10-25 19:31:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,12 +9,12 @@ import numpy as np
 import os
 import importlib
 
-# Load sentences & embeddings from disc
-with open('clinical_inno_embeddings_masterid_paraphrase-multilingual-mpnet-base-v2.pkl', "rb") as fIn:
+#Load sentences & embeddings from disc
+with open('mean_clinical_inno_embeddings_masterid_paraphrase-multilingual-mpnet-base-v2.pkl', "rb") as fIn:
     stored_data = pickle.load(fIn)
     stored_masterid = stored_data['pro_master_id']
-    stored_products = stored_data['products']
-    stored_embeddings = stored_data['embeddings']
+    stored_products = stored_data['mean_products']
+    stored_embeddings = stored_data['mean_embeddings']
 
 # Initialize the SentenceTransformer model
 embedder = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
@@ -27,24 +27,6 @@ def get_similar_products(query, products, mean_embeddings_tensor, top_k=10):
     similar_products = [(products[idx], score.item()) for score, idx in zip(top_results[0], top_results[1])]
     return similar_products
 
-# Create a DataFrame using the loaded data
-df = pd.DataFrame({
-    'master_id': stored_masterid,
-    'product': stored_products,
-    'embeddings': list(map(np.array, stored_embeddings))  # Convert each embedding to a numpy array
-})
-
-def compute_mean_embedding(group):
-    # Ensure that 'embeddings' is a list of numpy arrays
-    embeddings = np.stack(group['embeddings'].to_numpy())
-    # Compute and return the mean embedding
-    return np.mean(embeddings, axis=0)
-
-# Group by 'master_id' column and compute the mean embeddings
-mean_embeddings = df.groupby('master_id').apply(compute_mean_embedding)
-
-# Convert the list of numpy arrays to a tensor
-mean_embeddings_tensor = torch.tensor(np.stack(mean_embeddings))
 
 
 # Streamlit UI
@@ -54,6 +36,6 @@ st.title("Product Similarity Finder")
 user_query = st.text_area("Enter your query:", "SuperSole Bred læst er en utrolig...")
 
 if st.button("Find Similar Products"):
-    results = get_similar_products(user_query, stored_products, mean_embeddings_tensor)
+    results = get_similar_products(user_query, stored_products, stored_embeddings)
     for product, score in results:
         st.write(f"Product: {product} (Score: {score:.4f})")
